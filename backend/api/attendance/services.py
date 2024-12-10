4bc478d0ac89aa0c6d6c175e39b7b51d77d775aa
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2024-12-10 22:28:48 +0700

    1

--- backend/api/attendance/services.py
+++ backend/api/attendance/services.py
@@ -3,40 +3,40 @@ from flask import jsonify
 import sqlite3
 import requests
 
-score_board_URL = "https://sheetdb.io/api/v1/ma8opp1ci2oqd"
+attendance_URL = "https://sheetdb.io/api/v1/tgpag8379teow"
 
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
     return connection
 
-def get_score_board_admin(request):
-    response = requests.get(score_board_URL)
+def get_attendance_admin(request):
+    response = requests.get(attendance_URL)
     if response.status_code == 200:
         data = response.json()
 
         # connection = get_db_connection()
         # cursor = connection.cursor()
 
-        # cursor.execute("DELETE FROM score_boards")
+        # cursor.execute("DELETE FROM attendances")
 
         # for item in data:
         #     cursor.execute(
-        #         "INSERT INTO score_boards (id, name, score) VALUES (?, ?, ?)",
+        #         "INSERT INTO attendances (id, name, score) VALUES (?, ?, ?)",
         #         (item['id'], item['name'], item['score']) 
         #     )
 
         # connection.commit()
         # connection.close()
         return jsonify({
-            "message": "Score board fetched successfully!",
+            "message": "Attendance fetched successfully!",
             "data": data
         }), 200
     else:
         return jsonify({"message": f"Error occurred: {response.status_code}"}), 500
     
-def get_score_board_users(request):
-    response = requests.get(score_board_URL)
+def get_attendance_users(request):
+    response = requests.get(attendance_URL)
     if response.status_code == 200:
         data = response.json()
         request_body = request.json
@@ -49,19 +49,19 @@ def get_score_board_users(request):
 
             # for item in student_data:
             #     # Kiểm tra xem bản ghi với ID đã tồn tại chưa
-            #     cursor.execute("SELECT COUNT(*) FROM score_boards WHERE id = ?", (item['id'],))
+            #     cursor.execute("SELECT COUNT(*) FROM attendances WHERE id = ?", (item['id'],))
             #     exists = cursor.fetchone()[0]
 
             #     if exists:
             #         # Cập nhật bản ghi nếu đã tồn tại
             #         cursor.execute(
-            #             "UPDATE score_boards SET name = ?, score = ? WHERE id = ?",
+            #             "UPDATE attendances SET name = ?, score = ? WHERE id = ?",
             #             (item['name'], item['score'], item['id'])
             #         )
             #     else:
             #         # Thêm bản ghi nếu chưa tồn tại
             #         cursor.execute(
-            #             "INSERT INTO score_boards (id, name, score) VALUES (?, ?, ?)",
+            #             "INSERT INTO attendances (id, name, score) VALUES (?, ?, ?)",
             #             (item['id'], item['name'], item['score'])
             #         )
 
@@ -69,7 +69,7 @@ def get_score_board_users(request):
             # connection.close()
 
             return jsonify({
-            "message": "Score board fetched successfully!",
+            "message": "Attendance fetched successfully!",
             "data": student_data
         }), 200
         else:
--- backend/api/score_board/routes.py
+++ backend/api/score_board/routes.py
@@ -4,7 +4,7 @@ from . import score_board_bp
 
 @score_board_bp.route('/score_board_admin', methods=['GET'])
 def get_score_board_admin_route():
-    return get_score_board_admin()
+    return get_score_board_admin(request)
 
 @score_board_bp.route('/score_board_users', methods=['GET'])
 def get_score_board_users_route():
