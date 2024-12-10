2c55cee2d9082bfd448883993f2bf1cec128256f
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2024-12-10 22:37:52 +0700

    1

--- backend/api/attendance/services.py
+++ backend/api/attendance/services.py
@@ -15,19 +15,19 @@ def get_attendance_admin(request):
     if response.status_code == 200:
         data = response.json()
 
-        # connection = get_db_connection()
-        # cursor = connection.cursor()
+        connection = get_db_connection()
+        cursor = connection.cursor()
 
-        # cursor.execute("DELETE FROM attendances")
+        cursor.execute("DELETE FROM attendances")
 
-        # for item in data:
-        #     cursor.execute(
-        #         "INSERT INTO attendances (id, name, score) VALUES (?, ?, ?)",
-        #         (item['id'], item['name'], item['score']) 
-        #     )
+        for item in data:
+            cursor.execute(
+                "INSERT INTO attendances (id, name, score) VALUES (?, ?, ?)",
+                (item['id'], item['name'], item['score']) 
+            )
 
-        # connection.commit()
-        # connection.close()
+        connection.commit()
+        connection.close()
         return jsonify({
             "message": "Attendance fetched successfully!",
             "data": data
@@ -44,29 +44,29 @@ def get_attendance_users(request):
         student_data = [record for record in data if record.get("Mã sinh viên") == id]
     
         if student_data:
-            # connection = get_db_connection()
-            # cursor = connection.cursor()
+            connection = get_db_connection()
+            cursor = connection.cursor()
 
-            # for item in student_data:
-            #     # Kiểm tra xem bản ghi với ID đã tồn tại chưa
-            #     cursor.execute("SELECT COUNT(*) FROM attendances WHERE id = ?", (item['id'],))
-            #     exists = cursor.fetchone()[0]
+            for item in student_data:
+                # Kiểm tra xem bản ghi với ID đã tồn tại chưa
+                cursor.execute("SELECT COUNT(*) FROM attendances WHERE id = ?", (item['id'],))
+                exists = cursor.fetchone()[0]
 
-            #     if exists:
-            #         # Cập nhật bản ghi nếu đã tồn tại
-            #         cursor.execute(
-            #             "UPDATE attendances SET name = ?, score = ? WHERE id = ?",
-            #             (item['name'], item['score'], item['id'])
-            #         )
-            #     else:
-            #         # Thêm bản ghi nếu chưa tồn tại
-            #         cursor.execute(
-            #             "INSERT INTO attendances (id, name, score) VALUES (?, ?, ?)",
-            #             (item['id'], item['name'], item['score'])
-            #         )
+                if exists:
+                    # Cập nhật bản ghi nếu đã tồn tại
+                    cursor.execute(
+                        "UPDATE attendances SET name = ?, score = ? WHERE id = ?",
+                        (item['name'], item['score'], item['id'])
+                    )
+                else:
+                    # Thêm bản ghi nếu chưa tồn tại
+                    cursor.execute(
+                        "INSERT INTO attendances (id, name, score) VALUES (?, ?, ?)",
+                        (item['id'], item['name'], item['score'])
+                    )
 
-            # connection.commit()
-            # connection.close()
+            connection.commit()
+            connection.close()
 
             return jsonify({
             "message": "Attendance fetched successfully!",
--- backend/api/score_board/services.py
+++ backend/api/score_board/services.py
@@ -15,19 +15,20 @@ def get_score_board_admin(request):
     if response.status_code == 200:
         data = response.json()
 
-        # connection = get_db_connection()
-        # cursor = connection.cursor()
+        connection = get_db_connection()
+        cursor = connection.cursor()
 
-        # cursor.execute("DELETE FROM score_boards")
+        cursor.execute("DELETE FROM score_boards")
 
-        # for item in data:
-        #     cursor.execute(
-        #         "INSERT INTO score_boards (id, name, score) VALUES (?, ?, ?)",
-        #         (item['id'], item['name'], item['score']) 
-        #     )
+        for item in data:
+            cursor.execute(
+                "INSERT INTO score_boards (msv, fullname, score) VALUES (?, ?, ?)",
+                (item['id'], item['name'], item['score']) 
+            )
 
-        # connection.commit()
-        # connection.close()
+        connection.commit()
+        connection.close()
+
         return jsonify({
             "message": "Score board fetched successfully!",
             "data": data
@@ -44,29 +45,29 @@ def get_score_board_users(request):
         student_data = [record for record in data if record.get("Mã sinh viên") == id]
     
         if student_data:
-            # connection = get_db_connection()
-            # cursor = connection.cursor()
+            connection = get_db_connection()
+            cursor = connection.cursor()
 
-            # for item in student_data:
-            #     # Kiểm tra xem bản ghi với ID đã tồn tại chưa
-            #     cursor.execute("SELECT COUNT(*) FROM score_boards WHERE id = ?", (item['id'],))
-            #     exists = cursor.fetchone()[0]
+            for item in student_data:
+                # Kiểm tra xem bản ghi với ID đã tồn tại chưa
+                cursor.execute("SELECT COUNT(*) FROM score_boards WHERE id = ?", (item['id'],))
+                exists = cursor.fetchone()[0]
 
-            #     if exists:
-            #         # Cập nhật bản ghi nếu đã tồn tại
-            #         cursor.execute(
-            #             "UPDATE score_boards SET name = ?, score = ? WHERE id = ?",
-            #             (item['name'], item['score'], item['id'])
-            #         )
-            #     else:
-            #         # Thêm bản ghi nếu chưa tồn tại
-            #         cursor.execute(
-            #             "INSERT INTO score_boards (id, name, score) VALUES (?, ?, ?)",
-            #             (item['id'], item['name'], item['score'])
-            #         )
+                if exists:
+                    # Cập nhật bản ghi nếu đã tồn tại
+                    cursor.execute(
+                        "UPDATE score_boards SET name = ?, score = ? WHERE id = ?",
+                        (item['name'], item['score'], item['id'])
+                    )
+                else:
+                    # Thêm bản ghi nếu chưa tồn tại
+                    cursor.execute(
+                        "INSERT INTO score_boards (id, name, score) VALUES (?, ?, ?)",
+                        (item['id'], item['name'], item['score'])
+                    )
 
-            # connection.commit()
-            # connection.close()
+            connection.commit()
+            connection.close()
 
             return jsonify({
             "message": "Score board fetched successfully!",
