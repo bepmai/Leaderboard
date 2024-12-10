bb1bb2bd4b26a9cd2f649a60b3a167158d005018
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2024-12-10 21:31:24 +0700

    1

---
+++ backend/api/score_board/routes.py
@@ -0,0 +1,11 @@
+from flask import request
+from .services import get_score_board_admin, get_score_board_users
+from . import score_board_bp
+
+@score_board_bp.route('/score_board_admin', methods=['GET'])
+def get_score_board_admin_route():
+    return get_score_board_admin()
+
+@score_board_bp.route('/score_board_users', methods=['GET'])
+def get_score_board_users_route():
+    return get_score_board_users(request)
