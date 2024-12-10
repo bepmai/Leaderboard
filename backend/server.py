7b1ae0ec4de9ca4fcd55829a65d691a0925de8f4
thanhst <thanhxk2003@gmail.com>
2024-12-10 20:41:31 +0700

    update

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from api.auth import auth_bp
+from api.infor import infor_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(auth_bp, url_prefix='/auth')
+app.register_blueprint(infor_bp, url_prefix='/infor')
 
 if __name__ == '__main__':
     app.run(debug=True)
