468774fcec5687d9de0af2492e3410681ce1f6a1
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2024-12-11 08:54:46 +0700

    1

--- backend/api/score_board/services.py
+++ backend/api/score_board/services.py
@@ -23,7 +23,7 @@ def get_score_board_admin(request):
         for item in data:
             cursor.execute(
                 "INSERT INTO score_boards (msv, stt, first_name,last_name,class,Go_to_the_board,Summarize_Mindmap,code_sytem,Total) VALUES (?, ?, ?,?, ?, ?,?, ?, ?)",
-                (item['STT'], item['Mã sinh viên'], item['Họ '],item['Tên'], item['Lớp'], item['Lên bảng'],item['Mindmap tổng hợp'], item['Code hệ thống'],item['Tổng điểm tích cực']) 
+                (item['Mã sinh viên'], item['STT'], item['Họ '],item['Tên'], item['Lớp'], item['Lên bảng'],item['Mindmap tổng hợp'], item['Code hệ thống'],item['Tổng điểm tích cực']) 
             )
 
         connection.commit()
@@ -56,14 +56,14 @@ def get_score_board_users(request):
                 if exists:
                     # Cập nhật bản ghi nếu đã tồn tại
                     cursor.execute(
-                        "UPDATE score_boards SET name = ?, score = ? WHERE id = ?",
-                        (item['name'], item['score'], item['id'])
+                        "UPDATE score_boards SET stt = ?, first_name = ?,last_name = ?,class = ?,Go_to_the_board = ?,Summarize_Mindmap = ?,code_sytem = ?,Total = ? WHERE msv = ?",
+                        (item['STT'], item['Họ '],item['Tên'], item['Lớp'], item['Lên bảng'],item['Mindmap tổng hợp'], item['Code hệ thống'],item['Tổng điểm tích cực'],item['Mã sinh viên']) 
                     )
                 else:
                     # Thêm bản ghi nếu chưa tồn tại
                     cursor.execute(
-                        "INSERT INTO score_boards (id, name, score) VALUES (?, ?, ?)",
-                        (item['id'], item['name'], item['score'])
+                        "INSERT INTO score_boards (msv, stt, first_name,last_name,class,Go_to_the_board,Summarize_Mindmap,code_sytem,Total) VALUES (?, ?, ?,?, ?, ?,?, ?, ?)",
+                        (item['Mã sinh viên'], item['STT'], item['Họ '],item['Tên'], item['Lớp'], item['Lên bảng'],item['Mindmap tổng hợp'], item['Code hệ thống'],item['Tổng điểm tích cực']) 
                     )
 
             connection.commit()
