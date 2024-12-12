679b5b2bccf53100642f88b72fed7e149c654431
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2024-12-12 17:26:21 +0700

    1

--- backend/api/dashboard/services.py
+++ backend/api/dashboard/services.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request 
 from flask import jsonify
 import sqlite3
+from datetime import datetime
 import requests
 
 def get_db_connection():
@@ -10,33 +11,37 @@ def get_db_connection():
 
 def get_dashboard_info_admin(request):
     try:
-        with get_db_connection() as connection:  # Context manager for connection
-            cursor = connection.cursor()  # Manually manage the cursor
-            try:
-                # Fetch school attendance rate
-                cursor.execute("SELECT COUNT(*) FROM attendances WHERE absent <= 3")
-                school_attendance_rate = cursor.fetchone()[0]
-                
-                # Fetch break rate
-                cursor.execute("SELECT COUNT(*) FROM attendances WHERE absent > 3")
-                break_rate = cursor.fetchone()[0]
-                
-                # Fetch number of speeches (cluster)
-                cursor.execute("SELECT SUM(Total) FROM score_boards")
-                number_of_speeches_cluster = cursor.fetchone()[0]
-                
-                # Fetch number of speeches (class)
-                cursor.execute("SELECT SUM(stated) FROM attendances")
-                number_of_speeches_class = cursor.fetchone()[0]
-            finally:
-                connection.close()  # Ensure the cursor is closed after use
+        date_strings = ["12/11/2024", "15/11/2024", "22/11/2024", "26/11/2024","29/11/2024","03/12/2024","06/12/2024","10/12/2024","13/12/2024","17/12/2024","20/12/2024","24/12/2024","27/12/2024"]
+
+        date_objects = [datetime.strptime(date, "%d/%m/%Y") for date in date_strings]
+
+        today = datetime.now()
+        day = 1
+        for date in date_objects:
+            if today > date:
+                day+=1
+            else:
+                break
+        print(day)
+        connection = get_db_connection()
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT COUNT(*) FROM attendance_of_day WHERE absent != ? AND day = ?", ('v', day))
+        number_of_attendance = cursor.fetchone()[0]
+        
+        cursor.execute("SELECT COUNT(*) FROM attendance_of_day WHERE absent = ? AND day = ?", ('v', day))
+        number_of_break = cursor.fetchone()[0]
+        
+        cursor.execute("SELECT SUM(stated) FROM attendances")
+        number_of_speeches = cursor.fetchone()[0]
+
+        connection.close()
         
         return jsonify({
             "message": "Dashboard info fetched successfully!",
-            "School_attendance_rate": school_attendance_rate,
-            "Break_rate": break_rate,
-            "Number_of_speeches_cluster": number_of_speeches_cluster,
-            "Number_of_speeches_class": number_of_speeches_class
+            "number_of_attendance": number_of_attendance,
+            "number_of_break": number_of_break,
+            "number_of_speeches": number_of_speeches
         }), 200
     except Exception as e:
         return jsonify({
@@ -55,7 +60,7 @@ def get_dashboard_info_users(request):
                 number_of_school_attendance = cursor.fetchone()[0]
                 
                 # Fetch break rate
-                cursor.execute("SELECT COUNT(*) FROM attendance_of_day WHERE absent == ? AND msv = ?", ('v', id))
+                cursor.execute("SELECT COUNT(*) FROM attendance_of_day WHERE absent = ? AND msv = ?", ('v', id))
                 number_of_break = cursor.fetchone()[0]
                 
                 # Fetch number of speeches (cluster)
@@ -83,7 +88,7 @@ def get_dashboard_info_users(request):
                     rank = 0
 
             finally:
-                connection.close()  # Ensure the cursor is closed after use
+                cursor.close()  # Ensure the cursor is closed after use
         
         return jsonify({
             "message": "Dashboard info fetched successfully!",
