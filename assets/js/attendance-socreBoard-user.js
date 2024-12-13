ed2fb960862497b55dfdb4d7e8e59b4bdfca2c25
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2024-12-13 12:14:36 +0700

    1

--- assets/js/attendance-socreBoard-user.js
+++ assets/js/attendance-socreBoard-user.js
@@ -57,7 +57,52 @@ async function fetchAttendaceUsers() {
     }
   }
 
-  fetchAttendaceUsers();
+//   fetchAttendaceUsers();
+
+async function fetchSoreBoardUsers() {
+    try {
+      const msv = getCookie('msv');
+  
+      if (!msv) {
+          console.error("Token không tồn tại trong cookie!");
+          return;
+      }
+      const response = await fetch(`http://localhost:5000/api/score_board/score_board_users?id=${msv}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          credentials:'include'
+      });
+  
+      const data = await response.json();
+  
+      console.log("Dữ liệu nhận được:", data);
+
+      const tableBody = document.getElementById("SoreBoardTable"); // Lấy phần <tbody> của bảng
+
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+            <td>${data['data'][0]['STT']}</td>
+            <td>${data['data'][0]['Mã sinh viên']}</td>
+            <td>${data['data'][0]['Họ ']} ${data['data'][0]['Tên']}</td>
+            <td>${data['data'][0]['Lớp']}</td>
+            <td>${data['data'][0]['Lên bảng']}</td>
+            <td>${data['data'][0]['Mindmap tổng hợp']}</td>
+            <td>${data['data'][0]['Code hệ thống']}</td>
+            <td>${data['data'][0]['Tổng điểm tích cực']}</td>
+        `;
+
+        tableBody.appendChild(row);
+
+    } catch (error) {
+      console.error("Lỗi khi gọi API:", error);
+      document.getElementById("errorMessage").textContent = "Không thể tải dữ liệu.";
+    }
+  }
+
+  fetchSoreBoardUsers();
 
   function getCookie(name) {
     const value = `; ${document.cookie}`;
--- backend/api/attendance/services.py
+++ backend/api/attendance/services.py
@@ -112,66 +112,7 @@ def get_attendance_users(request):
         student_data = [record for record in data if record.get("Mã sinh viên") == id]
     
         if student_data:
-            connection = get_db_connection()
-            cursor = connection.cursor()
-
-            for item in student_data:
-                # Kiểm tra xem bản ghi với ID đã tồn tại chưa
-                cursor.execute("SELECT COUNT(*) FROM attendances WHERE msv = ?", (item['Mã sinh viên'],))
-                exists = cursor.fetchone()[0]
-
-                cursor.execute("DELETE FROM attendance_of_day WHERE msv = ?", (item['Mã sinh viên'],))
-
-                if exists:
-                    # Cập nhật bản ghi nếu đã tồn tại
-                    cursor.execute(
-                        "UPDATE attendances SET stt = ?, first_name = ?,last_name = ?,class = ?,project_point = ?,note = ?,absent = ?,stated = ? WHERE msv = ?",
-                        (item['STT'], item['Họ'],item['Tên'], item['Lớp'], item['Điểm project'],item['Ghi chú'], item['Vắng'],item['Phát biểu'],item['Mã sinh viên']) 
-                    )
-                    for i in range(1,16):
-                        if (item[f'{i}']=='v' or item[f'{i}']=='pb'):
-                            if (item[f'{i}']=='pb'):
-                                cursor.execute(
-                                    "INSERT INTO attendance_of_day (msv, day,stated,absent) VALUES (?, ?, ?,?)",
-                                    (item['Mã sinh viên'], i, 1,'') 
-                                )
-                            else:
-                                cursor.execute(
-                                    "INSERT INTO attendance_of_day (msv, day,stated,absent) VALUES (?, ?, ?,?)",
-                                    (item['Mã sinh viên'], i, '',item[f'{i}']) 
-                                )
-                        else:
-                            cursor.execute(
-                                    "INSERT INTO attendance_of_day (msv, day,stated,absent) VALUES (?, ?, ?,?)",
-                                    (item['Mã sinh viên'], i, '','') 
-                                )
-                else:
-                    cursor.execute(
-                        "INSERT INTO attendances (msv, stt, first_name,last_name,class,project_point,note,absent,stated) VALUES (?, ?, ?,?, ?, ?,?, ?, ?)",
-                        (item['Mã sinh viên'], item['STT'], item['Họ'],item['Tên'], item['Lớp'], item['Điểm project'],item['Ghi chú'], item['Vắng'],item['Phát biểu']) 
-                    )
-                    cursor.execute("DELETE FROM attendance_of_day WHERE msv = ?", (item['Mã sinh viên'],))
-                    
-                    for i in range(1,16):
-                        if (item[f'{i}']=='v' or item[f'{i}']=='pb'):
-                            if (item[f'{i}']=='pb'):
-                                cursor.execute(
-                                    "INSERT INTO attendance_of_day (msv, day,stated,absent) VALUES (?, ?, ?,?)",
-                                    (item['Mã sinh viên'], i, 1,'') 
-                                )
-                            else:
-                                cursor.execute(
-                                    "INSERT INTO attendance_of_day (msv, day,stated,absent) VALUES (?, ?, ?,?)",
-                                    (item['Mã sinh viên'], i, '',item[f'{i}']) 
-                                )
-                        else:
-                            cursor.execute(
-                                    "INSERT INTO attendance_of_day (msv, day,stated,absent) VALUES (?, ?, ?,?)",
-                                    (item['Mã sinh viên'], i, '','') 
-                                )
-
-            connection.commit()
-            connection.close()
+            
 
             return jsonify({
             "message": "Attendance fetched successfully!",
